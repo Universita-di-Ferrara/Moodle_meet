75c39771e3258b13d3e472ac7a8d1df0a5a1b2e6
BertelliAndrea <berteandrea@gmail.com>
2024-03-19 10:25:25 +0100

    update version.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_gmeet';
 $plugin->release = '0.1.0';
-$plugin->version = 2024031201;
+$plugin->version = 2024031901;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
