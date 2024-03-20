28b395c6a6306a0e35ffc230a251df75e35cc6ce
BertelliAndrea <berteandrea@gmail.com>
2024-03-20 09:23:49 +0100

    releasing in beta and update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_gmeet';
-$plugin->release = '0.1.0';
-$plugin->version = 2024031904;
+$plugin->release = '0.2.0';
+$plugin->version = 2024032001;
 $plugin->requires = 2022112800;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_BETA;
